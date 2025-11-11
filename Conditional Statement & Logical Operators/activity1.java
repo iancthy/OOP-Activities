4da20bbb655a44a7558692fc0dba5f9972a1fcb5
Gian Carlo Sanchez <164422423+iancthy@users.noreply.github.com>
2025-11-11 23:24:11 +0800

    Add even/odd checker in activity1.java

--- Conditional Statement & Logical Operators/activity1.java
+++ Conditional Statement & Logical Operators/activity1.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+class activity1{
+    public static void main(String[] args)  {
+    Scanner scanner = new Scanner(System.in);
+        
+    System.out.print("Input a number: ");
+    double num1 = scanner.nextDouble();
+    
+    if (num1 % 2 == 0){
+        System.out.println("Even");
+    } else{
+        System.out.println("Odd");
+     
+        }   
+    }
+}   
