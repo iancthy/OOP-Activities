5fe37c10b82a72368274656c67cd9253b794e1be
Gian Carlo Sanchez <164422423+iancthy@users.noreply.github.com>
2025-11-11 22:32:15 +0800

    Add user input for name and favorite quote

--- Taking Input/activity1.java
+++ Taking Input/activity1.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+class activity1{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        
+        System.out.print("What is your favorite quote? ");
+        String quote = scanner.nextLine();
+        
+        System.out.println("Hello there " + name + "! Your favorite quote is: " + quote);
+    }
+}
