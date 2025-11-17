2d1ce2ca0033b2c0dad2e36023a9b5cd53297bb3
Gian Carlo Sanchez <164422423+iancthy@users.noreply.github.com>
2025-11-17 16:03:13 +0800

    Update and rename activity1.java to activity2.java

---
+++ Arrays/activity2.java
@@ -0,0 +1,17 @@
+public class activity2 {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 45, 3, 78, 32, 56};
+
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum value: " + max);
+    }
+}
+
